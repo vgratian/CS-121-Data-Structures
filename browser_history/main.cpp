52eca3066869b01609d6447f7a224d4211400375
vgratian <vgratian@utopianlab.am>
2016-09-20 23:39:58 +0400

    fully functional; next step: make oop

--- browser_history/main.cpp
+++ browser_history/main.cpp
@@ -3,20 +3,25 @@
 #include <vector>
 using namespace std;
 
-/*
-void print_history(string history[], int position, int size) {
+void print_history(vector<string> history, int position) {
 
-    if(position==1) {
-        cout << "previous: EMPTY" << endl;
-    }
+    if(position<1) { ; } // No history
     else {
         cout << "previous: " << endl;
-        for(int i = 0; i < size-1; i++) {
-            cout << " " << history[i] << endl;
+        for(int i = position-1; i >=0; i--) {
+            cout << "<" << history[i] << endl;
+        }
+    }
+
+    if(position+1==history.size()) { ; } // No forward links
+    else {
+        cout << "forward: " << endl;
+        for(int i = position+1; i < history.size(); i++) {
+            cout << ">" << history[i] << endl;
         }
     }
     return;
-}*/
+}
 
 int main() {
 
@@ -24,15 +29,19 @@ int main() {
     int position = -1;
     string input;
 
+    cout << endl <<
+        "Welcome to Quixster browser!" << endl <<
+        "Enter the url you want to visit" << endl <<
+        "Enter 'back' or 'next' to navigate" << endl <<
+        "Or 'end' to quit the browser" << endl;
+
     while(true) {
-        cout << "go to: ";
+        cout << endl << "go to: ";
         cin >> input;
 
-        //if(input=="end") { break; }
+        if(input=="end") { break; }
 
-        if(input=="back" || input=="previous") {
-            cout << "back!" << endl;
-
+        else if(input=="back" || input=="previous") {
             if(position>0) {
                 position--;
                 cout << "current website(" << position << "): " <<  history[position] << endl;
@@ -40,26 +49,22 @@ int main() {
             else { cout << "BACK IS EMPTY" << endl; }
         }
 
-/*        if(current=="next" || current=="forward") {
-            if(position<size-1) {
+        else if(input=="next" || input=="forward") {
+            if(position+1<history.size()) {
                 position++;
+                cout << "current website: " <<  history[position] << endl;
             }
-            else {
-                cout << "NEXT IS EMPTY" << endl;
-            }
-        } */
+            else { cout << "NEXT IS EMPTY" << endl; }
+        }
 
         else {
-
             position++;
             if(position < history.size()) { history.resize(position); }
-
             history.push_back(input);
-            cout << "push!" << endl;
             cout << "current website(" << position << "): " <<  history[position] << endl;
             }
 
-        //print_history(history, position, size);
+        print_history(history, position);
 
     }
 
