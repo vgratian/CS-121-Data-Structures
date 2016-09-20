28797a02185ec4a28fa3c03205f7c7cdf28d90d9
vgratian <vgratian@utopianlab.am>
2016-09-21 00:39:41 +0400

    moved functions/variables from main to a new class

--- browser_history/main.cpp
+++ browser_history/main.cpp
@@ -3,70 +3,86 @@
 #include <vector>
 using namespace std;
 
-void print_history(vector<string> history, int position) {
+class browsing_session {
+    public:
+        vector<string> history;
+        int position = -1; // because we have position++ BEFORE pushing inputted url to history
+        // so -1 will become 0 and the first url will become history[0]
+        string input;
 
-    if(position<1) { ; } // No history
-    else {
-        cout << "previous: " << endl;
-        for(int i = position-1; i >=0; i--) {
-            cout << "<" << history[i] << endl;
+        void welcome_screen() {
+            cout << endl <<
+                "Welcome to Quixster browser!" << endl <<
+                "Enter the url you want to visit" << endl <<
+                "Enter 'back' or 'next' to navigate" << endl <<
+                "Or 'end' to quit the browser" << endl;
         }
-    }
 
-    if(position+1==history.size()) { ; } // No forward links
-    else {
-        cout << "forward: " << endl;
-        for(int i = position+1; i < history.size(); i++) {
-            cout << ">" << history[i] << endl;
+        void run_session() {
+            while(true) {
+                cout << endl << "go to: ";
+                cin >> input;
+
+                if(input=="end") { break; }
+                else if(input=="back") { browse_back(history, &position); }
+                else if(input=="next") { browse_forward(history, &position); }
+                else {
+                    // the position of current website is move to next element
+                    position++;
+                    // if there are urls before our position they will be deleted by resizing the vector:
+                    if(position < history.size()) { history.resize(position); }
+                    // new inputted url is pushed into the history
+                    history.push_back(input);
+                    // new inputted url is printed
+                    cout << "current website: " <<  history[position] << endl;
+                    }
+
+                print_history(history, position);
+            }
         }
-    }
-    return;
-}
 
-int main() {
+        void print_history(vector<string> history, int position) {
+            if(position<1) { ; } // Skips if there are no back links
+            else {
+                cout << "previous: " << endl;
+                for(int i = position-1; i >=0; i--) {
+                    cout << " " << history[i] << endl;
+                }
+            }
 
-    vector<string> history;
-    int position = -1;
-    string input;
+            if(position+1==history.size()) { ; } // Skips if there are no forward links
+            else {
+                cout << "forward: " << endl;
+                for(int i = position+1; i < history.size(); i++) {
+                    cout << " " << history[i] << endl;
+                }
+            }
+            return;
+        }
 
-    cout << endl <<
-        "Welcome to Quixster browser!" << endl <<
-        "Enter the url you want to visit" << endl <<
-        "Enter 'back' or 'next' to navigate" << endl <<
-        "Or 'end' to quit the browser" << endl;
-
-    while(true) {
-        cout << endl << "go to: ";
-        cin >> input;
-
-        if(input=="end") { break; }
-
-        else if(input=="back" || input=="previous") {
-            if(position>0) {
-                position--;
-                cout << "current website(" << position << "): " <<  history[position] << endl;
+        void browse_back(vector<string> history, int *position) {
+            if(*position>0) {
+                *position -= 1;
+                cout << "current website: " <<  history[*position] << endl;
                 }
             else { cout << "BACK IS EMPTY" << endl; }
         }
 
-        else if(input=="next" || input=="forward") {
-            if(position+1<history.size()) {
-                position++;
-                cout << "current website: " <<  history[position] << endl;
+        void browse_forward(vector<string> history, int *position) {
+            if(*position+1<history.size()) {
+                *position += 1;
+                cout << "current website: " <<  history[*position] << endl;
             }
             else { cout << "NEXT IS EMPTY" << endl; }
         }
+};
 
-        else {
-            position++;
-            if(position < history.size()) { history.resize(position); }
-            history.push_back(input);
-            cout << "current website(" << position << "): " <<  history[position] << endl;
-            }
 
-        print_history(history, position);
+int main() {
 
-    }
+    browsing_session a;
+    a.welcome_screen();
+    a.run_session();
 
     return 0;
 }
