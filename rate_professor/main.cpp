b5ac2dc30e70fa30dfacb4171db37d56ae695e3e
imandes0 <vgratian@utopianlab.am>
2016-12-13 23:01:28 +0400

    headers in main.cpp

--- rate_professor/main.cpp
+++ rate_professor/main.cpp
@@ -1,56 +1,14 @@
 #include <iostream>
 #include <string>
-
-class Professor {
-private:
-  string name;
-  string courses[];
-  int rating;
-  void update_rating();
-
-public:
-  Professor();
-  int get_rating();
-  string get_name();
-
-};
-
-class Student {
-private:
-  string name;
-  string email;
-  string hash_password;
-  string courses[];
-
-public:
-  Student();
-  string get_name;
-
-};
-
-class Rating() {
-private:
-  int rating;
-  string description;
-  string to_professor;
-  string from_student;
-
-public:
-  Rating();
-};
-
-class Course() {
-private:
-  string name;
-  string professor;
-  string students;
-
-public:
-  Course();
-}
-
+#include "professor.cpp"
+#include "student.cpp"
+#include "course.cpp"
+#include "rating.cpp"
+#include "session.cpp"
 
 main() {
 
+  Session hello;
+  
   return 0;
 }
