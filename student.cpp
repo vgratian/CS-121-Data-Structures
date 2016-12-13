92ac934d7ec6ce9f5565508cf31b6019adb98231
imandes0 <vgratian@utopianlab.am>
2016-12-13 22:22:11 +0400

    divided into several files

---
+++ student.cpp
@@ -0,0 +1,12 @@
+class Student {
+private:
+  std::string name;
+  std::string email;
+  std::string hash_password;
+  std::string courses[100];
+
+public:
+  Student();
+  std::string get_name;
+
+};
