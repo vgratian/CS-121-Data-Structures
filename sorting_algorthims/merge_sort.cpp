761ffc4b178c9b7a108cfd81b379b07a8c16d00e
imandes0 <vgratian@utopianlab.am>
2016-12-15 06:14:40 +0400

    cleanup, comment about bug

--- sorting_algorthims/merge_sort.cpp
+++ sorting_algorthims/merge_sort.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-#include "main.h"
-using namespace std;
-
+/*.
+Sorts an array of integers using the Merge Sort algorithm
+TODO: FIX BUG, probably in lines 21-24
+*/
 
 void merge(int *main_arr, int size, int *left_arr, int *right_arr) {
-  cout << "merge ! " << endl;
   int k, j, i;
   k = 0; j = 0; i = 0;
 
-  while(i < (size/2) && j < (size/2)) {
+  while(i < (size/2) && j < (size/2) {
     if(left_arr[i] < right_arr[j])
       main_arr[k++] = left_arr[i++];
     else
@@ -19,15 +18,11 @@ void merge(int *main_arr, int size, int *left_arr, int *right_arr) {
     main_arr[k++] = left_arr[i++];
   while(j < (size/2))
     main_arr[k++] = right_arr[j++];
-
-    print_array(main_arr, 9);
 }
 
-void mergesort(int *arr, int size) {
+void merge_sort(int *arr, int size) {
     if(size < 2)
     return;
-
-  cout << "split ! " << endl;
 
   int mid = size/2;
   int *left;
@@ -41,25 +36,8 @@ void mergesort(int *arr, int size) {
   for(int i=mid; i<size; i++)
     right[(i-mid)] = arr[i];
 
-  print_array(left, mid);
-  print_array(right, size-mid);
-
-  mergesort(left, mid);
-  mergesort(right, size-mid);
+  merge_sort(left, mid);
+  merge_sort(right, size-mid);
 
   merge(arr, size, left, right);
-}
-
-int main() {
-
-  int size = 9;
-  int unsorted[size] = {2, 4, 7, 20, 1, 3, 5, 12, 6};
-
-  print_array(unsorted, size);
-  mergesort(unsorted, size);
-
-//  print_array(unsorted, size);
-  cout << "it works...!" << endl;
-
-  return 0;
 }
